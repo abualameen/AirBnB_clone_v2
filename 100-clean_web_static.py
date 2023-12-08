62674f9929caa062264cd39f94e79b6219eed6ef
abu al ameen <abulyaqs@gmail.com>
2023-12-08 23:02:59 +0100

    newss

---
+++ 100-clean_web_static.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Fabric script that deletes out-of-date archives.
+"""
+
+from fabric.api import env, local
+from fabric.context_managers import cd
+from fabric.operations import sudo
+
+
+env.hosts = ['<IP web-01>', '<IP web-02>']
+env.user = 'ubuntu'  # Update with your username
+env.key_filename = '~/.ssh/id_rsa'  # Update with your private key path
+
+
+def do_clean(number=0):
+    """
+    Delete out-of-date archives.
+
+    Args:
+        number (int): The number of archives to keep.
+
+    Returns:
+        None
+    """
+    number = int(number)
+
+    if number == 0 or number == 1:
+        number = 1
+    else:
+        number += 1
+
+    # Delete unnecessary archives in the versions folder
+    with cd("/data/web_static/releases"):
+        local("ls -1t versions | tail -n +{} | xargs -I {{}}\
+                rm versions/{{}}".format(number))
+
+    # Delete unnecessary archives in the /data/web_static/releases folder
+    with cd("/data/web_static/releases"):
+        sudo("ls -1t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number))
