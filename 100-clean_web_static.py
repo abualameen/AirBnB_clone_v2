d8d885b1685a830e95684f9be3a6315858079414
abu al ameen <abulyaqs@gmail.com>
2023-12-09 15:11:47 +0100

    correction

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,7 +4,6 @@ Fabric script that deletes out-of-date archives.
 """
 
 from fabric.api import run, local, env, task
-from datetime import datetime
 
 env.hosts = ['35.153.33.61', '54.165.26.200']
 env.user = 'ubuntu'  # Update with your username
@@ -30,10 +29,12 @@ def do_clean(number=0):
         number = 2
 
     # Delete local archives
-    local("ls -1t versions | tail -n +{} | xargs -I {{}} rm versions/{{}}".format(number + 1))
+    local("ls -1t versions | tail -n +{} | xargs -I {{}}\
+            rm versions/{{}}".format(number + 1))
 
     # Delete remote archives
-    run("ls -1t /data/web_static/releases | tail -n +{} | xargs -I {{}} rm -rf /data/web_static/releases/{{}}".format(number + 1))
+    run("ls -1t /data/web_static/releases | tail -n +{} | xargs -I {{}}\
+            rm -rf /data/web_static/releases/{{}}".format(number + 1))
 
     # Remove the "total" line from the local ls output
     local("ls -ltr versions | grep -v '^total'")
@@ -44,7 +45,8 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
 
     parser = ArgumentParser(description="Delete out-of-date archives.")
-    parser.add_argument("number", type=int, help="Number of archives to keep. Default is 0.")
+    parser.add_argument("number", type=int,
+                        help="Number of archives to keep. Default is 0.")
 
     args = parser.parse_args()
     # Call the do_clean task with the specified number of archives to keep
