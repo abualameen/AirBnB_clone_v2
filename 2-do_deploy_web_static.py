93c484b80aec52a19fd2bf4df7af99985d58c4bc
abu al ameen <abulyaqs@gmail.com>
2023-12-09 12:51:20 +0100

    corrections

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,7 +11,6 @@ env.hosts = ['35.153.33.61', '54.165.26.200']
 env.user = 'ubuntu'  # Update with your username
 env.key_filename = '~/.ssh/id_rsa'  # Update with your private key path
 
-
 def do_deploy(archive_path):
     """
     Distributes an archive to your web servers.
@@ -60,7 +59,6 @@ def do_deploy(archive_path):
     except Exception as e:
         print(e)
         return False
-
 
 if __name__ == "__main__":
     current_time = datetime.utcnow()
