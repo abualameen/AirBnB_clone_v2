0d655bd7b9f2e7d6d4973f9fb20ea4464c50ef22
abu al ameen <abulyaqs@gmail.com>
2023-12-09 15:27:52 +0100

    nwiiii

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,67 +1,46 @@
-# Puppet manifest for setting up web servers for web_static deployment
+# puppet/manifests/init.pp
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
+# Install Nginx
+class { 'nginx':
+  ensure => 'installed',
 }
 
-# Create necessary directories
-file { '/data':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static/releases':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static/shared':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static/releases/test/':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+# Create necessary folders
+file { ['/data/web_static/releases/test/', '/data/web_static/shared/']:
+  ensure => 'directory',
 }
 
 # Create a fake HTML file for testing
 file { '/data/web_static/releases/test/index.html':
-  content => "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>",
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+  ensure  => 'file',
+  content => '<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>',
+  require => File['/data/web_static/releases/test/'],
 }
 
-# Create or recreate symbolic link
+# Create symbolic link
 file { '/data/web_static/current':
-  ensure => link,
+  ensure => 'link',
   target => '/data/web_static/releases/test/',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+}
+
+# Give ownership of the /data/ folder to the ubuntu user and group recursively
+file { '/data/':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
 }
 
 # Update Nginx configuration
-file_line { 'nginx_config':
-  path    => '/etc/nginx/sites-available/default',
-  line    => ' \ \n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}',
-  match   => 'server_name _;',
-  require => Package['nginx'],
+file { '/etc/nginx/sites-available/default':
+  ensure => 'file',
+  content => "location /hbnb_static {\n\talias /data/web_static/current/;\n}\n",
+  notify => Service['nginx'],
 }
 
 # Restart Nginx
 service { 'nginx':
-  ensure    => running,
+  ensure    => 'running',
   enable    => true,
-  subscribe => File_line['nginx_config'],
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
