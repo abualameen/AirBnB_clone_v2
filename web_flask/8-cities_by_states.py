51ffbed2a9d394a76e2478fd34422c81cab8aa3f
abu al ameen <abulyaqs@gmail.com>
2023-12-29 21:45:14 +0100

    done

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,12 +7,10 @@ from models.city import City
 # Initialize Flask application
 app = Flask(__name__)
 
-
 @app.teardown_appcontext
 def teardown_db(exception):
     """Close the current SQLAlchemy session."""
     storage.close()
-
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
@@ -21,11 +19,7 @@ def cities_by_states():
     cities = storage.all(City).values()
     states = [state for state in states if isinstance(state, State)]
     cities = [city for city in cities if isinstance(city, City)]
-    return render_template(
-                            '8-cities_by_states.html',
-                            states=states,
-                            cities=cities)
-
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
